9aaa8ef62b4344d8739ec61ea0cb2e26e91cb7b2
Op-Pautu <pautuop@gmail.com>
2023-11-04 01:11:17 +0530

    create upvote & downvote actions

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -43,7 +43,7 @@ const Page = async ({ params }: any) => {
           </Link>
           <div className="flex justify-end">
             <Votes
-              type="question"
+              type="Q uestion"
               itemId={JSON.stringify(result._id)}
               userId={JSON.stringify(mongoUser._id)}
               upvotes={result.upvotes.length}
--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import {
+  downvoteQuestion,
+  upvoteQuestion,
+} from "@/lib/actions/question.action";
 import { formatAndDivideNumber } from "@/lib/utils";
 import Image from "next/image";
+import { usePathname, useRouter } from "next/navigation";
+import { type } from "os";
 import React from "react";
 
 interface VotesProps {
@@ -25,9 +31,59 @@ const Votes = ({
   hasDownvoted,
   hasSaved,
 }: VotesProps) => {
+  const pathname = usePathname();
+  const router = useRouter();
+
   const handleSave = () => {};
 
-  const handleVote = (action: string) => {};
+  const handleVote = async (action: string) => {
+    if (!userId) {
+      return;
+    }
+
+    if (action === "upvote") {
+      if (type === "Question") {
+        await upvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasUpvoted,
+          hasDownvoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        // await upvoteAnswer({
+        //   questionId: JSON.parse(itemId),
+        //   userId: JSON.parse(userId),
+        //   hasUpvoted,
+        //   hasDownvoted,
+        //   path: pathname,
+        // });
+      }
+      // todo: show a toast
+      return;
+    }
+
+    if (action === "downvote") {
+      if (type === "Question") {
+        await downvoteQuestion({
+          questionId: JSON.parse(itemId),
+          userId: JSON.parse(userId),
+          hasUpvoted,
+          hasDownvoted,
+          path: pathname,
+        });
+      } else if (type === "Answer") {
+        // await downvoteAnswer({
+        //   questionId: JSON.parse(itemId),
+        //   userId: JSON.parse(userId),
+        //   hasUpvoted,
+        //   hasDownvoted,
+        //   path: pathname,
+        // });
+      }
+      // todo: show a toast
+    }
+  };
 
   return (
     <div className="flex gap-5">
