8b2902df9aad6ddc20a7795ee41f3bd341012ced
Op-Pautu <pautuop@gmail.com>
2023-11-09 14:18:06 +0530

    implement pagination on home page

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -2,7 +2,6 @@
 
 import React from "react";
 import { Button } from "../ui/button";
-import { boolean } from "zod";
 import { formUrlQuery } from "@/lib/utils";
 import { useRouter, useSearchParams } from "next/navigation";
 
